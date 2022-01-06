5b0b867b615a5c71f7c8ba77e0b0392b7ff28110
ajay <ajaymittal7231@gmail.com>
2022-01-06 19:44:00 +0530

    test

--- crypto.py
+++ crypto.py
@@ -2,7 +2,7 @@
 # yahoo finance to get the crypto data
 import streamlit as st
 from datetime import date
-# from currencyconverter import CurrencyConverter 
+import requests
 
 import yfinance as yf
 from fbprophet import Prophet
@@ -10,67 +10,124 @@ from fbprophet import Prophet
 from fbprophet.plot import plot_plotly
 from plotly import graph_objs as go
 
+url = 'https://api.exchangerate-api.com/v4/latest/USD'
 
-START = "2015-01-01" # start date
-TODAY = date.today().strftime("%Y-%m-%d") # today's date and convert it into string format
+# Get the data from the API
 
-# currencyConverter = CurrencyConverter()
 
-st.title("Crypto Prediction App") # title of the app
+class RealTimeCurrencyConverter():
+    def __init__(self, url):
+        self.data = requests.get(url).json()
+        self.currencies = self.data['rates']
+    # Get the conversion rate
 
-# currencies = st.selectbox("Select currencies", currencyConverter.get_currency_list())
+    def convert(self, from_currency, to_currency, amount):
+        initial_amount = amount
+        # first convert it into USD if it is not in USD.
+        # because our base currency is USD
+        if from_currency != 'USD':
+            amount = amount / self.currencies[from_currency]
+
+        # limiting the precision to 4 decimal places
+        amount = round(amount * self.currencies[to_currency], 4)
+        return amount
+
+
+START = "2015-01-01"  # start date
+# today's date and convert it into string format
+TODAY = date.today().strftime("%Y-%m-%d")
+
+currencyConverter = RealTimeCurrencyConverter(url)
+
+st.title("Crypto Prediction App")  # title of the app
+
+currencies = ["USD",
+              "EUR",
+              "GBP",
+              "AUD",
+              "CAD",
+              "CHF",
+              "CNY",
+              "DKK",
+              "HKD",
+              "INR",
+              "JPY",
+              "KRW",
+              "NZD",
+              "PLN",
+              "RUB",
+              "SEK",
+              "SGD",
+              "THB",
+              "TRY"]
+
 
 cryptos = ("BTC-USD", "ETH-USD", "BNB-USD",
            "USDT-USD", "HEX-USD", "SOL-USD", "DOGE-USD", "SHIB-USD")
 
-selected_cryptos = st.selectbox("Select dataset for prediction", cryptos) # select the crypto from the select box and return it into selected_cryptos variable
+# select the crypto from the select box and return it into selected_cryptos variable
+selected_cryptos = st.selectbox("Select dataset for prediction", cryptos)
+currencies = st.selectbox("Select currencies", currencies)
 
-n_years = st.slider("Years Of prediction:", 1, 4) # slider to select the number of years of prediction from 1 to 4
+# slider to select the number of years of prediction from 1 to 4
+n_years = st.slider("Years Of prediction:", 1, 4)
 
-period = n_years * 365 # number of days in a year
+period = n_years * 365  # number of days in a year
 
-@st.cache # store the donwloaded data in the cache so we don't have to download it again
-def load_data(ticker): # ticker is basically the selected_cryptos variable
-    data = yf.download(ticker, START, TODAY) # to download the data from yahoo finance for a specified start and end date
-    data.reset_index(inplace=True) # This puts date in the very first column
+
+@st.cache  # store the donwloaded data in the cache so we don't have to download it again
+def load_data(ticker):  # ticker is basically the selected_cryptos variable
+    # to download the data from yahoo finance for a specified start and end date
+    data = yf.download(ticker, START, TODAY)
+    data.reset_index(inplace=True)  # This puts date in the very first column
     return data
 
-data_load_state = st.text("Loading data...") # text to show the loading of data
 
-data = load_data(selected_cryptos) # synchronous call to function
-data_load_state.text("Loading data...done!") # after the data is loaded, we can plot the raw data
+# text to show the loading of data
+data_load_state = st.text("Loading data...")
 
-st.subheader('Raw Data') # subheader for raw data
-st.write(data.tail()) # display the last 5 rows of the data
+data = load_data(selected_cryptos)  # synchronous call to function
+# after the data is loaded, we can plot the raw data
+data_load_state.text("Loading data...done!")
+
+st.subheader('Raw Data')  # subheader for raw data
+st.write(data.tail())  # display the last 5 rows of the data
+
 
 def plot_raw_data():
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name='crypto_open'))
-    fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name='crypto_close'))
-    fig.layout.update(title_text="Time Series Data", xaxis_rangeslider_visible=True)
+    fig.add_trace(go.Scatter(x=data['Date'],
+                  y=data['Open'], name='crypto_open'))
+    fig.add_trace(go.Scatter(x=data['Date'],
+                  y=data['Close'], name='crypto_close'))
+    fig.layout.update(title_text="Time Series Data",
+                      xaxis_rangeslider_visible=True)
     st.plotly_chart(fig)
-         
+
+
 plot_raw_data()
 
 # Forecasting()
 df_train = data[['Date', 'Close']]
-df_train = df_train.rename(columns={"Date": "ds", "Close": "y"}) # we have to rename because prophet expects the column names to be ds and y
+# we have to rename because prophet expects the column names to be ds and y
+df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
 
 
-m = Prophet() # Creating a model
-m.fit(df_train) # fitting the training data
+m = Prophet()  # Creating a model
+m.fit(df_train)  # fitting the training data
 
-future = m.make_future_dataframe(periods=period) # dataframe for the future data ; period is the number of days we want to predict
-forecast = m.predict(future) # predicting the future data
+# dataframe for the future data ; period is the number of days we want to predict
+future = m.make_future_dataframe(periods=period)
+forecast = m.predict(future)  # predicting the future data
 
-st.subheader('Forecast data') # subheader for forecast data
-st.write(forecast.tail()) # display the last 5 rows of the forecast data
+st.subheader('Forecast data')  # subheader for forecast data
+st.write(forecast.tail())  # display the last 5 rows of the forecast data
 
 
-st.write('forecast data') # display the forecast data
-fig1 = plot_plotly(m, forecast) # plot the forecast data
+st.write('forecast data')  # display the forecast data
+fig1 = plot_plotly(m, forecast)  # plot the forecast data
 st.plotly_chart(fig1)   # plot the forecast data
 
-st.write('forecast components') # display the forecast components
-fig2 = m.plot_components(forecast) # plot the forecast components
-st.write(fig2) # plot the forecast components
+st.write('forecast components')  # display the forecast components
+fig2 = m.plot_components(forecast)  # plot the forecast components
+st.write(fig2)  # plot the forecast components
